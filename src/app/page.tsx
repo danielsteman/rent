dc7eee9cccca9ac9e26f42f00ecafa05495dea8d
Daniel Steman <daniel-steman@live.nl>
2024-01-16 22:03:01 +0100

    added some icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 const Navbar = ({ toggleMenu }: { toggleMenu: () => void }) => {
@@ -11,14 +10,25 @@ const Navbar = ({ toggleMenu }: { toggleMenu: () => void }) => {
           <a href="#" className="text-xl font-bold mr-auto">
             Logo
           </a>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              width="17.5"
+              viewBox="0 0 448 512"
+              fill="white"
+            >
+              <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+            </svg>
+          </button>
           <button
             onClick={toggleMenu}
             className="text-white focus:outline-none lg:hidden"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              width="21"
+              height="20"
+              width="17.5"
               viewBox="0 0 448 512"
               fill="white"
             >
